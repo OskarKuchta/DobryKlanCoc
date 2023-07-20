368d1f08eb04cf0029fc810c02db6a1c6025b0af
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-07-20 17:52:17 +0200

    Set filter changes, but once filter not work well ;/

--- app/src/components/Stats.tsx
+++ app/src/components/Stats.tsx
@@ -9,46 +9,38 @@ const Stats: React.FC = () => {
   }
   const filterOptions: string[] = ["NAZWA", "ROLA", "LEVEL", "PUCHARY"];
   const filterList = filterOptions;
-  const [filterLoop, setfilterLoop] = useState<number>(0);
+  const [filterLoop, setfilterLoop] = useState<number>(1);
   const { data } = useFetch();
   const statsButton = useRef(null);
   const statsBtn = statsButton.current;
   if (data) {
     const filterCurrent: () => void = () => {
       setfilterLoop((prev) => prev + 1);
-      if (filterLoop === 2) {
+      if (filterLoop === 3) {
         setfilterLoop(0);
       }
       statsBtn.blur();
     };
-    const sortedAlphabetically = [...data.memberList].sort(
-      (a: Member, b: Member) => a.name.localeCompare(b.name)
-    );
-
-    const sortedByExp = [...data.memberList].sort(
-      (a: Member, b: Member) => b.expLevel - a.expLevel
-    );
-
-    const sortedByTrophy = [...data.memberList].sort(
-      (a: Member, b: Member) => b.trophies - a.trophies
-    );
-    const sortedByRole = (a: Member, b: Member) => {
-      const roleOrder: { [key: string]: number } = {
-        Leader: 3,
-        "Co-leader": 2,
-        Elder: 1,
-        Member: 0,
-      };
-
-      return roleOrder[b.role] - roleOrder[a.role];
+    const comparator = (a: Member, b: Member) => {
+      if (filterLoop === 0) {
+        return a.name.localeCompare(b.name);
+      } else if (filterLoop === 1) {
+        
+      } else if (filterLoop === 2) {
+        return b.expLevel - a.expLevel;
+      } else if (filterLoop === 3) {
+        return b.trophies - a.trophies;
+      } else {
+        return b.expLevel - a.expLevel;
+      }
     };
-    console.log(data.memberList[0]);
+    const sortedList = [...data.memberList].sort(comparator);
     return (
       <div id="stats" className="stats">
         <div className="stats__top">
           <h2>Lista graczy względem: {filterList[filterLoop]}</h2>
           <button onClick={filterCurrent} ref={statsButton}>
-            {filterList[filterLoop + 1 <= 2 ? filterLoop + 1 : 0]}
+            {filterList[filterLoop + 1 <= 3 ? filterLoop + 1 : 0]}
           </button>
         </div>
         <div className="stats__members">
@@ -63,7 +55,7 @@ const Stats: React.FC = () => {
               </tr>
             </thead>
             <tbody>
-              {sortedAlphabetically.map((element: Member, index: number) => (
+              {sortedList.map((element: Member, index: number) => (
                 <tr key={index}>
                   <td>{index + 1}</td>
                   <td>{element.name}</td>
