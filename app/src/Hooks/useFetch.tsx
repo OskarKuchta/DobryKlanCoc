6de9f0fb70d01b5c40edcdfe8628de033275893e
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-07-23 12:25:14 +0200

    Delete vite config server

--- app/src/Hooks/useFetch.tsx
+++ app/src/Hooks/useFetch.tsx
@@ -18,7 +18,7 @@ const useFetch = (): Fetch => {
   const [data, setData] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
-  const url: string = "api/v1/clans/%23Y09R909";
+  const url: string = "https://api.clashofclans.com/v1/clans/%23Y09R909";
   const token: string =
     "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImE3NjlkYTcxLTFhMWQtNDdmZC1iZTc4LTM1MTY5YmQ0NWYwNyIsImlhdCI6MTY4OTM2MjA0MSwic3ViIjoiZGV2ZWxvcGVyLzczZGY3Yjk0LThkNzktZWM0YS1iMjE0LWRlN2NiZmE2OTkxNiIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjE3OC4yMzUuMTg4LjI1NCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.X1urimcIB_d48VwnD9txf9BrQtNWDIFdE-WFrSyoNp2A7GuILB2T-pCfLPsaH1D0IIeEcs7YDCpkbT9TA2_mBg";
   const options: Options = {
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -5,13 +5,4 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   base: "./",
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://api.clashofclans.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
 })
