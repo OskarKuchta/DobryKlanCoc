47d1bf9a80fce00d82e80cb0848a8191c720532e
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-07-24 11:49:42 +0200

    Change new token

--- app/src/Hooks/useFetch.tsx
+++ app/src/Hooks/useFetch.tsx
@@ -19,8 +19,9 @@ const useFetch = (): Fetch => {
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
   const url: string = "api/v1/clans/%23Y09R909";
+  // const url: string = "https://api.clashofclans.com/v1/clans/%23Y09R909";
   const token: string =
-    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImE3NjlkYTcxLTFhMWQtNDdmZC1iZTc4LTM1MTY5YmQ0NWYwNyIsImlhdCI6MTY4OTM2MjA0MSwic3ViIjoiZGV2ZWxvcGVyLzczZGY3Yjk0LThkNzktZWM0YS1iMjE0LWRlN2NiZmE2OTkxNiIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjE3OC4yMzUuMTg4LjI1NCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.X1urimcIB_d48VwnD9txf9BrQtNWDIFdE-WFrSyoNp2A7GuILB2T-pCfLPsaH1D0IIeEcs7YDCpkbT9TA2_mBg";
+    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjI0MDViZDVlLTdmNzEtNDk3ZC1hODM4LWY0MzI2NzQwNDRmNyIsImlhdCI6MTY5MDE5MTExMSwic3ViIjoiZGV2ZWxvcGVyLzczZGY3Yjk0LThkNzktZWM0YS1iMjE0LWRlN2NiZmE2OTkxNiIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjE3OC4yMzUuMTg4LjI1NCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.5yl0ucurDoB4g0qXxfierPQV87xtSwnojL032q9Lqzck_yuPyS7RISArzlqD32JrJD48_w4253ammFh1ZnjYIg";
   const options: Options = {
     method: "GET",
     headers: {
