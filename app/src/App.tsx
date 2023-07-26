04f68032c0906359a12870dee591faccabb44a5e
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-07-26 18:04:14 +0200

    Another try to redirect api

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -16,8 +16,8 @@ const App: React.FC = () => {
   };
   const [main, setMain] = useState<boolean>(true);
   const [ak, setAk] = useState<boolean>(false);
-  const urlMain: string = "https://api.clashofclans.com/v1/clans/%23Y09R909";
-  const urlAk: string = "https://api.clashofclans.com/v1/clans/%232qupvlcgc";
+  const urlMain: string = "api/v1/clans/%23Y09R909";
+  const urlAk: string = "api/v1/clans/%232qupvlcgc";
 
   // const url: string = "api/v1/clans/%23Y09R909";
   return (
