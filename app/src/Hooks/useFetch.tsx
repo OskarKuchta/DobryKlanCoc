bc3a1b00d0f1ca7a6b65c0d57c7aa341156fce2f
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-07-27 15:38:59 +0200

    Add abort controller after fetch

--- app/src/Hooks/useFetch.tsx
+++ app/src/Hooks/useFetch.tsx
@@ -7,39 +7,41 @@ interface Fetch {
   error: string | null;
 }
 
-interface Options {
-  method: string;
-  headers: {
-    Accept: string;
-    Authorization: string;
-  };
-}
 const useFetch = (url: string): Fetch => {
   const [data, setData] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
   const token: string =
     "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjY3OGIxYmRlLTUzOGEtNGI0Ni1iMjY0LTQzYTllZTRkNTQ2MiIsImlhdCI6MTY5MDM4NzMyMCwic3ViIjoiZGV2ZWxvcGVyLzczZGY3Yjk0LThkNzktZWM0YS1iMjE0LWRlN2NiZmE2OTkxNiIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjE3OC4yMzUuMTg4LjI1NCIsIjM0LjE0MS41NS4yNTAiXSwidHlwZSI6ImNsaWVudCJ9XX0.bs21JsEt0U7yRD_x4WGTJDPPTluH-j1vJpOlEbiUhjeC2_R84Jth5-1Wlj9pbrrHSjHPlA7UlIpZPJFKbo7G_w";
-  const options: Options = {
+  const options = {
     method: "GET",
     headers: {
       Accept: "application/json",
       Authorization: "Bearer " + token,
     },
   };
+
   useEffect(() => {
-    const fetchData: () => Promise<void> = async () => {
+    const fetchData = async () => {
+      const controller = new AbortController();
       try {
-        const response: AxiosResponse = await axios.get(url, options);
+        const response: AxiosResponse = await axios.get(url, {
+          ...options,
+          signal: controller.signal,
+        });
         setData(response.data);
         setLoading(false);
       } catch (error) {
         setError(error as string);
         setLoading(false);
       }
+      return () => {
+        controller.abort();
+      };
     };
     fetchData();
   }, [url]);
+
   return { data, loading, error };
 };
 
