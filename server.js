de0ecb34d82f125614f69c10ff736141704cf529
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-08-05 18:35:52 +0200

    Set universal IP with token, set correct server and fix some style

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors({
 app.get("/clans/main", async (req, res) => {
     try {
         const url = 'https://api.clashofclans.com/v1/clans/%23Y09R909';
-        const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImE5ZDY1MjlhLTE5NWUtNDQzNC1hMDdkLWVlOGVkMjczOWZkOCIsImlhdCI6MTY5MDk3ODkzMCwic3ViIjoiZGV2ZWxvcGVyLzczZGY3Yjk0LThkNzktZWM0YS1iMjE0LWRlN2NiZmE2OTkxNiIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjE3OC4yMzUuMTg4LjI1NCIsIjMuNjkuMjIuOTciLCIzNS4xNTkuMzMuMTkzIl0sInR5cGUiOiJjbGllbnQifV19.ZQCGRbgIC8mA2DV7BSsNIyz-QbEhb3kGLogJPumkEBhRniCkERv7WP8OktN5Klp0C8IG-fyAr4o8BU29dMC3Xg";
+        const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjI5OThlOWMzLTFlOTgtNDBiYS1iODFlLTcxZTk0NjhkM2FhMiIsImlhdCI6MTY5MTI1MTQ2MCwic3ViIjoiZGV2ZWxvcGVyLzczZGY3Yjk0LThkNzktZWM0YS1iMjE0LWRlN2NiZmE2OTkxNiIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjQ1Ljc5LjIxOC43OSJdLCJ0eXBlIjoiY2xpZW50In1dfQ.MfwLGOxHjlO1qy8N1Hh6LeX9bhAqcY0n_cNrY26Nctlg3R3GSIQOzkuwy6ngqGZnhCvEKOnFFnx6TuFOyfWNAw";
         const headers = {
             Accept: "application/json",
             Authorization: `Bearer ${token}`,
@@ -32,7 +32,7 @@ app.get("/clans/main", async (req, res) => {
 app.get("/clans/ak", async (req, res) => {
     try {
         const url = 'https://api.clashofclans.com/v1/clans/%232qupvlcgc';
-        const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImE5ZDY1MjlhLTE5NWUtNDQzNC1hMDdkLWVlOGVkMjczOWZkOCIsImlhdCI6MTY5MDk3ODkzMCwic3ViIjoiZGV2ZWxvcGVyLzczZGY3Yjk0LThkNzktZWM0YS1iMjE0LWRlN2NiZmE2OTkxNiIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjE3OC4yMzUuMTg4LjI1NCIsIjMuNjkuMjIuOTciLCIzNS4xNTkuMzMuMTkzIl0sInR5cGUiOiJjbGllbnQifV19.ZQCGRbgIC8mA2DV7BSsNIyz-QbEhb3kGLogJPumkEBhRniCkERv7WP8OktN5Klp0C8IG-fyAr4o8BU29dMC3Xg";
+        const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjI5OThlOWMzLTFlOTgtNDBiYS1iODFlLTcxZTk0NjhkM2FhMiIsImlhdCI6MTY5MTI1MTQ2MCwic3ViIjoiZGV2ZWxvcGVyLzczZGY3Yjk0LThkNzktZWM0YS1iMjE0LWRlN2NiZmE2OTkxNiIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjQ1Ljc5LjIxOC43OSJdLCJ0eXBlIjoiY2xpZW50In1dfQ.MfwLGOxHjlO1qy8N1Hh6LeX9bhAqcY0n_cNrY26Nctlg3R3GSIQOzkuwy6ngqGZnhCvEKOnFFnx6TuFOyfWNAw";
         const headers = {
             Accept: "application/json",
             Authorization: `Bearer ${token}`,
