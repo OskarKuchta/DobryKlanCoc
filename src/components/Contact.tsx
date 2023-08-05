79df3e2ab173982de9e5110ff987647fc81f52c0
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-08-06 00:03:25 +0200

    Start with contact component

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,7 +3,15 @@ import React from "react";
 const Contact: React.FC = () => {
   return (
     <div id="contact" className="contact">
-      <h3>Discord: 2v796kpsg5</h3>
+      <h2 className="contact__header">W razie pytań zapraszamy do kontaktu: </h2>
+      <h3>
+        Discord:{" "}
+        <a className="contact--first"
+          href="https://discord.com/channels/967750458858938418/973609130898108446"
+          target="blank">
+          Główny discord klanu
+        </a>
+      </h3>
     </div>
   );
 };
