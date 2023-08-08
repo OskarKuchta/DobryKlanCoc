7f1e0dd4959beb8db94b54f4e046d8fb4a4e7ee6
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-08-08 21:08:10 +0200

    Change photos format to better performance

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useRef, useState } from "react";
-import logo from "../assets/logo.png";
+import logo from "../assets/logo.webp";
 
 const Navbar: React.FC = () => {
   const [showMenu, setShowMenu] = useState<boolean>(false);
