c7d6b02e5698acf7e05fd430e1467aa7db052584
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-08-14 11:37:14 +0200

    Clean App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Navbar from "./components/Navbar";
 import { ErrorBoundary } from "./Hooks/useFetch";
 import Stats from "./components/Stats";
 import Contact from "./components/Contact";
 import AboutMain from "./components/AboutMain";
 import AboutAk from "./components/AboutAk";
+import ToogleURL from "./context/ToogleURL";
 const App: React.FC = () => {
-  const changeToMain = () => {
-    setAk(false);
-    setMain(true);
-  };
-  const changeToAk = () => {
-    setMain(false);
-    setAk(true);
-  };
-  const [showContact, setShowContact] = useState<boolean>(false);
-  useEffect(() => {
-    const load: NodeJS.Timeout = setTimeout(() => {
-      setShowContact(true);
-    }, 1500);
-    return () => {
-      clearTimeout(load);
-    };
-  });
-  const [main, setMain] = useState<boolean>(true);
-  const [ak, setAk] = useState<boolean>(false);
-  const urlMain: string =
-    "https://dobry-klan.netlify.app/.netlify/functions/server/clan/main";
-  const urlAk: string =
-    "https://dobry-klan.netlify.app/.netlify/functions/server/clan/ak"; // linki do serwera
-
-  // const urlMain: string = "api/v1/clans/%23Y09R909";
-  // const urlAk: string = "api/v1/clans/%232qupvlcgc"; // linki proxy
-
+  const { changeToMain, changeToAk, showContact, main, ak, urlMain, urlAk } =
+    ToogleURL();
   return (
     <div className="bg-wrapper">
       {main && (
