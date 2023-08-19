0e1359e27c5d3a4af7ce1040fd2dc2ba4f93cb89
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-08-19 13:01:10 +0200

    Add loading spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,60 @@
-import React from "react";
-import Navbar from "./components/Navbar";
+import React, { lazy, Suspense } from "react";
 import { ErrorBoundary } from "./Hooks/useFetch";
-import Stats from "./components/Stats";
-import Contact from "./components/Contact";
-import AboutMain from "./components/AboutMain";
-import AboutAk from "./components/AboutAk";
 import ToogleURL from "./context/ToogleURL";
+
+const AboutMain = lazy(() => import("./components/AboutMain"));
+const AboutAk = lazy(() => import("./components/AboutAk"));
+const Navbar = lazy(() => import("./components/Navbar"));
+const Stats = lazy(() => import("./components/Stats"));
+const Contact = lazy(() => import("./components/Contact"));
+
 const App: React.FC = () => {
   const { changeToMain, changeToAk, showContact, main, ak, urlMain, urlAk } =
     ToogleURL();
   return (
     <div className="bg-wrapper">
-      {main && (
-        <>
-          <Navbar />
-          <main>
-            <ErrorBoundary>
-              <AboutMain url={urlMain} onClick={changeToAk} />
-              <Stats url={urlMain} />
-            </ErrorBoundary>
-          </main>
-          {showContact && <Contact />}
-        </>
-      )}
-      {ak && (
-        <>
-          <Navbar />
-          <main>
-            <ErrorBoundary>
-              <AboutAk url={urlAk} onClick={changeToMain} />
-              <Stats url={urlAk} />
-            </ErrorBoundary>
-          </main>
-          {showContact && <Contact />}
-        </>
-      )}
+      <Suspense
+        fallback={
+          <div className="loading">
+            <div className="lds-roller">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            Loading...
+          </div>
+        }
+      >
+        {main && (
+          <>
+            <Navbar />
+            <main>
+              <ErrorBoundary>
+                <AboutMain url={urlMain} onClick={changeToAk} />
+                <Stats url={urlMain} />
+              </ErrorBoundary>
+            </main>
+            {showContact && <Contact />}
+          </>
+        )}
+        {ak && (
+          <>
+            <Navbar />
+            <main>
+              <ErrorBoundary>
+                <AboutAk url={urlAk} onClick={changeToMain} />
+                <Stats url={urlAk} />
+              </ErrorBoundary>
+            </main>
+            {showContact && <Contact />}
+          </>
+        )}
+      </Suspense>
     </div>
   );
 };
